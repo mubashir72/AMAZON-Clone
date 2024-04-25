bb4b37812939e83f2b2f415a9bcddcc2a3c0ca75
mubashir72 <mmubashirmemon@gmail.com>
2024-04-25 22:26:59 +0500

    updated app.js

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,21 +1,28 @@
 import React from "react";
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Hello Programmers, lets build amazon clone
-      </h1>
-      
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Hello Programmers, lets build amazon clone</h1>
+      </div>
+    </Router>
   );
 }
 
 export default App;
 
-{/* we need a react router */}
-{/* localhost.com */}
-{/* localhost.com/checkout */}
-{/* localhost.com/login */}
+{
+  /* we need a react router */
+}
+{
+  /* localhost.com */
+}
+{
+  /* localhost.com/checkout */
+}
+{
+  /* localhost.com/login */
+}
