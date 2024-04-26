b708410c2ce0d5ca31f203e21b9769f2128605eb
mubashir72 <mmubashirmemon@gmail.com>
2024-04-26 16:10:29 +0500

    updated header.js

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <nav className="header">
       {/* logo on the left */}
-      <img
-        className="header__logo"
-        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
       {/* search box */}
       {/* three links */}
       {/* basket icon */}
